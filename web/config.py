20b038b9307fd54512c2cd49afa38434d715c603
heiv82 <heiv82@gmail.com>
2021-07-22 20:31:04 +0200

    Update config.py

--- web/config.py
+++ web/config.py
@@ -4,7 +4,7 @@ import os
 class Configuration(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'sajdKWD&^*&@^#@6768726873WQT*T2'
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:1772735=-@db/db_dates'
+    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:<password>@db/db_dates'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     API_URL_BASE = "http://chirpstack-application-server:8080/"
